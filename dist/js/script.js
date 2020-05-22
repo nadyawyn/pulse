a1c8eaa51200309af52ad3a173bbae9dde1a2275
nadya <nadya.wyn@ya.ru>
2020-05-22 18:17:10 +0500

    Built DIST version

---
+++ dist/js/script.js
@@ -0,0 +1,143 @@
+$(document).ready(function () {
+	$('.carousel__content').slick({
+		autoplay: true,
+		infinite: true,
+		speed: 800,
+		slidesToShow: 1,
+		adaptiveHeight: false,
+		autoplaySpeed: 2000,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: false,
+					arrows: false
+				}
+			}
+		]
+	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+			})
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+	//Modal
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
+	//Validation 
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: "required",
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Please specify your name",
+				email: {
+					required: "We need your email address to contact you",
+					email: "Your email address must be in the format of name@domain.com"
+				}
+			}/*,
+			submitHandler: function (e) {
+				e.preventDefault();
+				$.ajax({
+					type: "POST",
+					url: "mailer/smart.php",
+					data: $(this).serialize()
+				}).done(function () {
+					$(this).find("input").val("");
+					$('#consultation, #order').fadeOut();
+					$('.overlay, #thanks').fadeIn('slow');
+					$('form').trigger('reset');
+				});
+				return false;
+			}*/
+		});
+	};
+
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
+
+	//Mask
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	//AJAX
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	//Smooth scroll and pageup
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn('slow');
+		}
+		else {
+			$('.pageup').fadeOut('slow');
+		}
+	});
+
+	$("a[href^='#up']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+
+	//Animation 
+	new WOW().init();
+
+});  
